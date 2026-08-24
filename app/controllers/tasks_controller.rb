723ecfd236d7c722950b4efec20aa2ccfc1bc2e9
John Fulton <john@John-Fultons-MacBook-Pro.local>
2026-08-24 11:42:46 -0400

    Add full CRUD: new/create, update via checkbox, delete/destroy via button

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,25 +1,40 @@
 class TasksController < ApplicationController
-
-def create
-  @task = Task.new(task_params)
-  @task.save
-  redirect_to tasks_path
-end
-
-def update
-  @task = Task.find(params[:id])
-end
-
-def index
-  @tasks = Task.all
-end
-
-def show
-  @task = Task.find(params[:id])
-end
-
-private
-  def task_params
-    params.require(:task).permit(:name,:completed)
+  def new
+    @task = Task.new
   end
+
+  def create
+    @task = Task.new(task_params)
+
+    if @task.save
+      redirect_to tasks_path
+    else
+      render :new
+    end
+  end
+
+  def update
+    @task = Task.find(params[:id])
+    @task.update(task_params)
+    redirect_to tasks_path
+  end
+
+  def index
+    @tasks = Task.all
+  end
+
+  def show
+    @task = Task.find(params[:id])
+  end
+
+  def destroy
+    @task = Task.find(params[:id])
+    @task.destroy
+    redirect_to tasks_path
+  end
+
+  private
+    def task_params
+      params.require(:task).permit(:name,:completed)
+    end
 end
