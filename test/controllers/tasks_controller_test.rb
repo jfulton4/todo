853ff83819bf23949e64b95421d29ff2c366012d
John Fulton <john@John-Fultons-MacBook-Pro.local>
2026-08-31 10:08:04 -0400

    added testing

--- test/controllers/tasks_controller_test.rb
+++ test/controllers/tasks_controller_test.rb
@@ -1,7 +1,54 @@
 require 'test_helper'
 
 class TasksControllerTest < ActionController::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  include Devise::Test::ControllerHelpers
+  setup do
+    @user = users(:one)
+    @other_user = users(:two)
+    @task = tasks(:one)
+
+    sign_in @user
+  end
+
+  test "redirects signed-out users to login" do
+    sign_out @user
+
+    get :index
+
+    assert_redirected_to new_user_session_path
+  end
+
+  test "creates a task for the current user" do
+    assert_difference('@user.tasks.count', 1) do
+      post :create, task: {
+        name: "Study controller tests",
+        completed: false
+      }
+    end
+
+    assert_redirected_to tasks_path
+    assert_equal @user, Task.order(:created_at).last.user
+  end
+
+  test "does not update another users task" do
+    other_task = tasks(:two)
+
+    assert_raises(ActiveRecord::RecordNotFound) do
+      patch :update,
+            id: other_task.id,
+            task: { completed: true }
+    end
+
+    assert_not other_task.reload.completed
+  end
+
+  test "does not delete another users task" do
+    other_task = tasks(:two)
+
+    assert_no_difference('Task.count') do
+      assert_raises(ActiveRecord::RecordNotFound) do
+        delete :destroy, id: other_task.id
+      end
+    end
+  end
 end
--- test/test_helper.rb
+++ test/test_helper.rb
@@ -8,3 +8,13 @@ class ActiveSupport::TestCase
 
   # Add more helper methods to be used by all tests here...
 end
+
+if RUBY_VERSION >= "2.6.0" && Rails.version < "5"
+  class ActionController::TestResponse < ActionDispatch::TestResponse
+    def recycle!
+      @mon_mutex_owner_object_id = nil
+      @mon_mutex = nil
+      initialize
+    end
+  end
+end
